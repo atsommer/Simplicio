70e35037706a8d6247ac8b8606ff32730bc1fd50
Ariel Sommer <atsommer@mit.edu>
2012-05-24 17:25:24 -0400

    Deleted icons

--- resource.h
+++ resource.h
@@ -2,11 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by app.rc
 //
-#define IDI_ICON4                       101
-#define IDI_ICON1                       102
-#define IDI_ICON2                       103
-#define IDI_ICON3                       104
-#define IDI_ICON0                       199
 
 // Next default values for new objects
 // 
